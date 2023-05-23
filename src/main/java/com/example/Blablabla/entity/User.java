b6b079c97e60a889d0c06ed39810ee6cf5d4361b
https://github.com/Resid-Huseynov/demo.git <residhuseynov763@gmail.com>
2023-05-23 11:58:48 +0400

    m

--- src/main/java/com/example/Blablabla/entity/User.java
+++ src/main/java/com/example/Blablabla/entity/User.java
@@ -40,17 +40,17 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
 
